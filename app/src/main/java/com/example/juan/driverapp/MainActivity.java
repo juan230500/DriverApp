dbde40577da538900f61b0e60ef4fbc647dd3c80
juan <juanpo.alvarado@gmail.com>
2018-11-14 21:17:38 -0600

    Bibliotecas para likendIn

--- app/src/main/java/com/example/juan/driverapp/MainActivity.java
+++ app/src/main/java/com/example/juan/driverapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.juan.driverapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.linkedin.platform.LISessionManager;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
