d46d39e3fced03e35a647aef8af1befe824c5cd3
José Solano <joseandressolanomora@gmail.com>
2018-11-21 11:50:05 -0600

    Manejo de ficheros y validaciones
    Se hace uso de ficheros para almacenar la información de los amigos agregados. Además se realizan validaciones de los datos que son escaneados.

--- app/src/main/java/com/example/juan/driverapp/CodigoBarras.java
+++ app/src/main/java/com/example/juan/driverapp/CodigoBarras.java
@@ -1,6 +1,7 @@
 package com.example.juan.driverapp;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.support.v4.app.ActivityCompat;
@@ -9,17 +10,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.Toast;
+
 import com.google.zxing.Result;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
 public class CodigoBarras extends AppCompatActivity implements ZXingScannerView.ResultHandler {
-    private EditText codigoBarras;
+    private String codigoBarras;
     private ZXingScannerView scannerView;
     private int codigoPermiso = 1;
 
-    public void Escanear(View view){
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED){
+
+    public void Escanear(View view) {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
             this.scannerView = new ZXingScannerView(this);
             setContentView(this.scannerView);
             scannerView.setResultHandler(this);
@@ -30,8 +39,8 @@ public class CodigoBarras extends AppCompatActivity implements ZXingScannerView.
 
     }
 
-    public void pedirPermiso(){
-        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)){
+    public void pedirPermiso() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
 
             new AlertDialog.Builder(this)
                     .setTitle("Permisos de cámara desactivados")
@@ -39,7 +48,7 @@ public class CodigoBarras extends AppCompatActivity implements ZXingScannerView.
                     .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
-                            ActivityCompat.requestPermissions(CodigoBarras.this, new String[] {Manifest.permission.CAMERA}, codigoPermiso);
+                            ActivityCompat.requestPermissions(CodigoBarras.this, new String[]{Manifest.permission.CAMERA}, codigoPermiso);
                         }
                     })
                     .setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
@@ -52,7 +61,7 @@ public class CodigoBarras extends AppCompatActivity implements ZXingScannerView.
 
 
         } else {
-            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, codigoPermiso);
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, codigoPermiso);
         }
     }
 
@@ -61,19 +70,123 @@ public class CodigoBarras extends AppCompatActivity implements ZXingScannerView.
         setContentView(R.layout.activity_codigo_barras);
     }
 
-    //protected void onPause() {
-    //    super.onPause();
-    //    scannerView.stopCamera();
-    //}
 
     public void handleResult(Result result) {
         scannerView.stopCamera();
         setContentView(R.layout.activity_codigo_barras);
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Resultado de escaneo");
-        builder.setMessage(result.getText());
-        AlertDialog alertDialog = builder.create();
-        alertDialog.show();
-        //scannerView.resumeCameraPreview(this);
+        if (result.getText().length() != 10) {
+            new AlertDialog.Builder(this)
+                    .setTitle("El codigo escaneado no pertenece a un carné TEC")
+                    .setMessage("Por favor intentelo de nuevo")
+                    .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    }).create().show();
+        } else {
+            this.codigoBarras = result.getText();
+            new AlertDialog.Builder(this)
+                    .setTitle("El carné de su amigo es")
+                    .setMessage(this.codigoBarras)
+                    .setPositiveButton("Correcto", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            archivar();
+                            dialog.dismiss();
+                        }
+                    })
+                    .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                            new AlertDialog.Builder(CodigoBarras.this)
+                                    .setTitle("Vuelva a intentarlo")
+                                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            dialog.dismiss();
+                                        }
+                                    }).create().show();
+                        }
+                    })
+                    .create().show();
+        }
+    }
+
+
+
+
+    public void archivar(){
+        String archivos []  = fileList();
+
+        if (archivoExiste(archivos, "amigos.txt")){
+            try {
+                InputStreamReader archivo_rd = new InputStreamReader(openFileInput("amigos.txt"));
+                BufferedReader br = new BufferedReader(archivo_rd);
+                String linea = br.readLine();
+                String lista_completa = "";
+
+                if (linea.equals(codigoBarras)){
+                    new AlertDialog.Builder(this)
+                            .setTitle("Este amigo ya ha sido agregado antes, pruebe con otro")
+                            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            }).create().show();
+                } else {
+
+                    while (linea != null) {
+                        lista_completa = lista_completa + linea + "\n";
+                        linea = br.readLine();
+                    }
+                    br.close();
+                    archivo_rd.close();
+                    guardar(lista_completa);
+                }
+
+            } catch (IOException e){
+
+            }
+        } else {
+            guardar("");
+        }
+
+    }
+
+    public void guardar(String codigos){
+        Toast.makeText(this, "aqui", Toast.LENGTH_LONG);
+        if (codigoBarras.length() == 10){
+            String codigos_nuevo = codigos + codigoBarras + "\n";
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Amigos");
+            builder.setMessage(codigos_nuevo);
+            AlertDialog alertDialog = builder.create();
+            alertDialog.show();
+
+            try {
+                OutputStreamWriter archivo_wr = new OutputStreamWriter(openFileOutput("amigos.txt", Activity.MODE_PRIVATE));
+                archivo_wr.write(codigos_nuevo);
+                archivo_wr.flush();
+                archivo_wr.close();
+            } catch (IOException e){
+
+            }
+            Toast.makeText(this, "Amigo agregado!", Toast.LENGTH_SHORT);
+        } else {
+            Toast.makeText(this, "Error al agregar amigo", Toast.LENGTH_LONG);
+        }
+
+    }
+
+    public boolean archivoExiste(String archivos [], String nombre){
+        for (int i = 0; i <archivos.length; i++) {
+            if (nombre.equals("amigos.txt")) {
+                return true;
+            }
+        }
+        return false;
     }
 }
