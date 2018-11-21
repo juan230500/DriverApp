9777d8ea5248c28a57069c03112e7c4cbea55596
sahid <sahid.rojas64@gmail.com>
2018-11-21 00:06:36 -0600

    Se agrega el espacio para insertar la residencia del conductor

--- app/src/main/java/com/example/juan/driverapp/MainActivity.java
+++ app/src/main/java/com/example/juan/driverapp/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.juan.driverapp;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+private EditText entrada;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        entrada=(EditText)findViewById(R.id.editText);
+
     }
+    public void SentGPS(View view){
+        Toast.makeText(MainActivity.this,
+                "Sent "+entrada.getText(), Toast.LENGTH_LONG).show();
+    }
+
 }
