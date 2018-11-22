b99e684d31908f39f0a597c362cb6452375048ef
sahid <sahid.rojas64@gmail.com>
2018-11-22 01:17:41 -0600

    Se crea la pantalla de calificacion para poder calificar a las personas

---
+++ app/src/main/java/com/example/juan/driverapp/Calificacion.java
@@ -0,0 +1,13 @@
+package com.example.juan.driverapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Calificacion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calificacion);
+    }
+}
