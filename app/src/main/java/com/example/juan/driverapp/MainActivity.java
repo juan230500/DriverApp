c0670f369d579a2ea89e2ab29c96298578a45cc9
sahid <sahid.rojas64@gmail.com>
2018-11-21 21:44:50 -0600

    Se agrega el boton sobre informacion de las coneciones de los vertices
    Mediante esta función de puede desde un vértice cuales son los vértices conectados a ese vértice y con que peso

--- app/src/main/java/com/example/juan/driverapp/MainActivity.java
+++ app/src/main/java/com/example/juan/driverapp/MainActivity.java
@@ -23,6 +23,8 @@ import java.util.Map;
 public class MainActivity extends AppCompatActivity {
 private EditText entrada;
 private int[][] Mapa;
+private String Matriz="";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +32,7 @@ private int[][] Mapa;
         setContentView(R.layout.activity_main);
         entrada=(EditText)findViewById(R.id.editText);
 
-        String REST_URI  = "http://192.168.100.4:8080/ServidorTEC/webapi/myresource/Mapa";
+        String REST_URI  = "http://192.168.100.22:8080/ServidorTEC/webapi/myresource/Mapa";
 
         RequestQueue requestQueue=Volley.newRequestQueue(this);
 
@@ -42,8 +44,9 @@ private int[][] Mapa;
                         // response
                         Gson gson=new Gson();
                         Mapa=gson.fromJson(response,int[][].class);
+                        Matriz=response;
                         Toast.makeText(MainActivity.this,
-                                "Sent "+response, Toast.LENGTH_LONG).show();
+                                "Sent hola "+response, Toast.LENGTH_LONG).show();
                     }
                 },
                 new Response.ErrorListener()
@@ -59,13 +62,40 @@ private int[][] Mapa;
         requestQueue.add(stringRequest);
 
     }
+    /*
+    1.Registrar residencia
+    2.Toast con caminos por el mapa
+    3.arrays con xy de los nodos
+    4.Bitacora XD
+     */
+    public void giveMeConexiones(View view){
+        int  response=Mapa[1][1];
+        if (!entrada.getText().equals("")){
+            String valor=entrada.getText().toString();
+            int nodo_a_Verificar=Integer.parseInt(valor);
+            String resultado="";
+            int Fila=30;
+            while (Fila!=0){
+                if (Mapa[Fila][nodo_a_Verificar]!=0){
+                    resultado+="=>"+Fila+"("+Mapa[Fila][nodo_a_Verificar]+")\n";
+                }
+                Fila-=1;
+            }
+            Toast.makeText(MainActivity.this,
+                    resultado, Toast.LENGTH_LONG).show();
 
+        }
+        else{
+            Toast.makeText(MainActivity.this,
+                    "Porfa llene el espacio de texto", Toast.LENGTH_LONG).show();
+        }
+    }
 
     public void SentGPS(View view){
         Toast.makeText(MainActivity.this,
                 "Sent "+entrada.getText(), Toast.LENGTH_LONG).show();
 
-        String REST_URI  = "http://192.168.100.4:8080/ServidorTEC/webapi/myresource/Residencia";
+        String REST_URI  = "http://192.168.100.22:8080/ServidorTEC/webapi/myresource/Residencia";
 
         RequestQueue requestQueue=Volley.newRequestQueue(this);
 
@@ -95,6 +125,7 @@ private int[][] Mapa;
             {
                 Map<String, String>  params = new HashMap<String, String>();
                 params.put("Residencia", ""+entrada.getText());
+                params.put("Carne","2018135360");
 
                 return params;
         }
